5a8c3d2b8a9ac6a9ee4fbe6575b50141076b6658
danhlc1204@gmail.com <danhlc1204@gmail.com>
2024-12-06 13:11:38 +0700

    Change from master branch

--- StoreManagement/ViewModels/CustomerViewModel.cs
+++ StoreManagement/ViewModels/CustomerViewModel.cs
@@ -238,9 +238,9 @@ namespace StoreManagement.ViewModels
         private void OnEdit(Customers customer)
         {
             //if (customer == null) return; 
-            
-            //var editCustomerView = new DashboardView
-            //{
+
+            //var editCustomerView = new DashboardView test2
+            //{ test2
             //    DataContext = new DashboardViewModel(_customerRepository, customer.Id)
             //};
 
