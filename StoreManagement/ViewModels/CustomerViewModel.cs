7cc51f1d33cf7396c10262121ca70b1ad9fda852
danhlc1204@gmail.com <danhlc1204@gmail.com>
2024-12-06 12:46:16 +0700

    test create conflict branch

--- StoreManagement/ViewModels/CustomerViewModel.cs
+++ StoreManagement/ViewModels/CustomerViewModel.cs
@@ -239,8 +239,8 @@ namespace StoreManagement.ViewModels
         {
             //if (customer == null) return; 
 
-            //var editCustomerView = new DashboardView
-            //{
+            //var editCustomerView = new DashboardView test1
+            //{ test1
             //    DataContext = new DashboardViewModel(_customerRepository, customer.Id)
             //};
 
